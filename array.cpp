7e55172f15b038697aa31c12d1ed3fa09786498d
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-07 18:07:18 +0700

    menambahkan akses dan ubah elemen array

--- array.cpp
+++ array.cpp
@@ -5,4 +5,11 @@ int main(){
     int dataku[5] = {10,20,30,40,50};
 
     int datamu[5];
+    
+    // Cara akses (membaca atau mengambil isi array) 
+    cout << "Isi data kedua : " << dataku[1] << endl;
+    
+    // Mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
 }
