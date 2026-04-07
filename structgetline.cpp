91059b5bc135d321f119d1891fea7ad58f02e7d4
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-07 19:05:12 +0700

    tambah struct detailAlamat dan Orang

---
+++ structgetline.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct detaiAlamat
+{
+    string desa;
+    string kota;
+
+};
+
+struct Orang
+{
+    string nama;
+    detaiAlamat alamat;
+    int umur;
+};
